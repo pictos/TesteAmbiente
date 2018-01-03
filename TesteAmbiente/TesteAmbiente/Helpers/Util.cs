1487a0cccb7cf56aa79296373d932375aa7dea00
Pedro Jesus <pedrojesus.cefet@gmail.com>
2018-01-03 00:39:44 -0200

    Adicionar arquivos de projeto.

---
+++ TesteAmbiente/TesteAmbiente/Helpers/Util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing.Net.Mobile.Forms;
+
+namespace TesteAmbiente.Helpers
+{
+    public class Util
+    {
+        public async static Task<ZXingScannerPage> Capturar(ZXingScannerPage scan,string titulo)
+        {
+            if(scan == null)
+            {
+                var options = new ZXing.Mobile.MobileBarcodeScanningOptions();
+
+#if __ANDROID__
+                MobileBarcodeScanner.Initialize(Application);
+#endif
+                scan = new ZXingScannerPage(options)
+                {
+                    IsScanning = true
+                };
+            }
+            scan.Title = titulo;
+            return scan;
+        }
+    }
+}
---
+++ TesteAmbiente/TesteAmbiente/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Tasks;
+using TesteAmbiente.Helpers;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace TesteAmbiente
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private string _codigo;
+
+        public string Codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+
+        private string _formato;
+
+        public string Formato
+        {
+            get { return _formato; }
+            set { _formato = value; }
+        }
+
+
+        bool init, msg = false;
+        ZXingScannerPage _scan = null;
+
+        async Task Capturar()
+        {
+            _scan = await Util.Capturar(_scan, "Escanear Código");
+            if(!init)
+            {
+                _scan.OnScanResult += (result) =>
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        try
+                        {
+                            if (msg)
+                                return;
+                            _scan.IsScanning = false;
+
+                            if (!string.IsNullOrEmpty(Codigo))
+                                return;
+                            Codigo = result.Text;
+                            Formato = result.BarcodeFormat.ToString();
+                            await Navigation.PopAsync();
+                        }
+                        catch (System.Exception ex)
+                        {
+
+                            msg = true;
+                            await DisplayAlert("Erro",ex.Message,"Ok");
+                            msg = false;
+                        }
+                    });
+                };
+                init = true;
+            }
+            Codigo = string.Empty;
+            await Navigation.PushAsync(_scan);
+        }
+
+        async void Button_Clicked(object sender, System.EventArgs e)
+        {
+            await Capturar();
+        }
+    }
+}
